2d5c13d12bbd24d05a9f88abaad833a64cd0871e
aexces <aexces@protonmail.com>
2022-03-25 04:56:23 +0530

    dependency injection added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mong/injectable.dart';
 
 import 'presentation/app_widget.dart';
 
 void main() {
+  configureInjection(Environment.prod);
   runApp(AppWidget());
 }
