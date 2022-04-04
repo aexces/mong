fa22b75670e65646ea303401bb87ae523dc03ffe
aexces <aexces@protonmail.com>
2022-04-04 12:32:33 +0530

    fonts updated

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,1 +1,9 @@
+//*Duration
+import 'package:flutter/cupertino.dart';
+
 const kDuration2Sec = Duration(seconds: 2);
+
+//*Spacing by SizedBox
+const kHeight10 = SizedBox(height: 10);
+const kHeight20 = SizedBox(height: 20);
+const kHeight30 = SizedBox(height: 30);
--- lib/presentation/pages/pending_debits/pending_debits_page.dart
+++ lib/presentation/pages/pending_debits/pending_debits_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mong/core/constants.dart';
 
 class PendingDebitsPage extends StatelessWidget {
   const PendingDebitsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: const [
+          kHeight20,
+          Text("Pending Debits"),
+        ],
+      ),
+    );
   }
 }
