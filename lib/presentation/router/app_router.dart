3fad88c23fdada846a7fad12a25ec81cf0869d86
aexces <aexces@protonmail.com>
2022-04-04 17:38:20 +0530

    add route

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/annotations.dart';
+import 'package:mong/presentation/pages/add/add_page.dart';
 
 import '../pages/pending/pending_page.dart';
 import '../pages/splash/splash_page.dart';
@@ -8,6 +9,7 @@ import '../pages/splash/splash_page.dart';
   routes: [
     AutoRoute(page: SplashPage, initial: true),
     AutoRoute(page: PendingPage),
+    AutoRoute(page: AddPage),
   ],
 )
 class $AppRouter {}
--- lib/presentation/router/app_router.gr.dart
+++ lib/presentation/router/app_router.gr.dart
@@ -10,38 +10,44 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:flutter/material.dart' as _i5;
 
+import '../pages/add/add_page.dart' as _i3;
 import '../pages/pending/pending_page.dart' as _i2;
 import '../pages/splash/splash_page.dart' as _i1;
 
-class AppRouter extends _i3.RootStackRouter {
-  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
+class AppRouter extends _i4.RootStackRouter {
+  AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i3.PageFactory> pagesMap = {
+  final Map<String, _i4.PageFactory> pagesMap = {
     SplashRoute.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
+      return _i4.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.SplashPage());
     },
     PendingRoute.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
+      return _i4.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i2.PendingPage());
+    },
+    AddRoute.name: (routeData) {
+      return _i4.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i3.AddPage());
     }
   };
 
   @override
-  List<_i3.RouteConfig> get routes => [
-        _i3.RouteConfig(SplashRoute.name, path: '/'),
-        _i3.RouteConfig(PendingRoute.name, path: '/pending-page')
+  List<_i4.RouteConfig> get routes => [
+        _i4.RouteConfig(SplashRoute.name, path: '/'),
+        _i4.RouteConfig(PendingRoute.name, path: '/pending-page'),
+        _i4.RouteConfig(AddRoute.name, path: '/add-page')
       ];
 }
 
 /// generated route for
 /// [_i1.SplashPage]
-class SplashRoute extends _i3.PageRouteInfo<void> {
+class SplashRoute extends _i4.PageRouteInfo<void> {
   const SplashRoute() : super(SplashRoute.name, path: '/');
 
   static const String name = 'SplashRoute';
@@ -49,8 +55,16 @@ class SplashRoute extends _i3.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.PendingPage]
-class PendingRoute extends _i3.PageRouteInfo<void> {
+class PendingRoute extends _i4.PageRouteInfo<void> {
   const PendingRoute() : super(PendingRoute.name, path: '/pending-page');
 
   static const String name = 'PendingRoute';
+}
+
+/// generated route for
+/// [_i3.AddPage]
+class AddRoute extends _i4.PageRouteInfo<void> {
+  const AddRoute() : super(AddRoute.name, path: '/add-page');
+
+  static const String name = 'AddRoute';
 }
