15bd959197799454fc75c320c1bfe02bc3300696
aexces <aexces@protonmail.com>
2022-04-18 22:41:15 +0530

    navigation completed

--- lib/presentation/pages/main/main_page.dart
+++ lib/presentation/pages/main/main_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mong/core/colors.dart';
 import 'package:mong/core/constants.dart';
 
+import 'widgets/custom_navigation.dart';
+
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -17,47 +19,7 @@ class MainPage extends StatelessWidget {
               ],
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 60,
-              decoration: const BoxDecoration(
-                borderRadius: kRadiusTL15TR15,
-                color: kPrimaryColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    iconSize: 30,
-                    icon: const Icon(
-                      Icons.account_balance_wallet_rounded,
-                      color: kWhiteColor,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    iconSize: 30,
-                    icon: const Icon(
-                      Icons.account_tree_rounded,
-                      color: kWhiteColor,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    iconSize: 30,
-                    icon: const Icon(
-                      Icons.settings_rounded,
-                      color: kWhiteColor,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          const CustomNavigation(),
         ],
       ),
     );
